2324b7431b074ba9360bb1aaf2be90bdc8fe39cb
thealphaversion <alphabuildv1@gmail.com>
2020-05-04 03:35:41 +0530

    setting up mongo

--- startup/database.js
+++ startup/database.js
@@ -3,8 +3,13 @@ const mongoose = require('mongoose');
 const config = require('config');
 
 module.exports = function() {
+    /*
     const db = config.get('db');
     mongoose.connect(db).then(() => {
         winston.info(`Connected to ${db} ...`);
+    });
+    */
+    mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/vidly').then(() => {
+        winston.info(`Connected to mongodb ...`);
     });
 }
